4dd1a93ec7ff995142b4dac2e0b8e4d3586c25d8
odes <odehse@mail.uc.edu>
2022-03-15 16:07:45 -0400

    add LoadAndDrawTH2 function implementation

--- example.C
+++ example.C
@@ -11,25 +11,32 @@ void example()
 
  //  the Get function returns a TObject pointer, so we have to cast it
  // to the type it actually is
-  TH1F* hEnu = (TH1F*)f->Get("validgenie/Enu");
-   if (!hEnu) {
-	 std::cerr << "hEnu not found! exiting" << std::endl;
-	 exit(1);
- 	 }
-  TH1F* hEnuNumu = (TH1F*)f->Get("validgenie/numu/Enu");
-  if (!hEnuNumu) {
-	 std::cerr << "hEnu not found! exiting" << std::endl;
-	 exit(1);
-	  }
+ 
+//  TH1F* hEnu = (TH1F*)f->Get("validgenie/Enu");
+//   if (!hEnu) {
+//	 std::cerr << "hEnu not found! exiting" << std::endl;
+//	 exit(1);
+// 	 }
+//  TH1F* hEnuNumu = (TH1F*)f->Get("validgenie/numu/Enu");
+//  if (!hEnuNumu) {
+//	 std::cerr << "hEnu not found! exiting" << std::endl;
+//	 exit(1);
+//	  }
 
-  // draw the histograms in different configurations, and save images
-  DrawTH1(hEnu, kOkabelto1, "plots/enu");
-  DrawTH1(hEnuNumu, kOkabelto2, "plots/enunumu");
+ // draw the histograms in different configurations, and save images
+
+//  DrawTH1(hEnu, kOkabelto1, "plots/enu");
+//  DrawTH1(hEnuNumu, kOkabelto2, "plots/enunumu");
 	 
   LoadAndDrawTH1(f,"validgenie/Enu", kOkabelto1, "plots/enu");
   LoadAndDrawTH1(f,"validgenie/numu/Enu", kOkabelto2, "plots/enunumu");
+ 
+ // draw 2d histogram
   
-  // recursively plot everything
-  // RecursivePlot(f);
+  LoadAndDrawTH2(f,"validgenie/Vxy", kOkabelto3, "plots/Vxy");
+
+ // recursively plot everything
+
+//   RecursivePlot(f);
 
 } // macro template
