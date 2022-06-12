d789dfaccd15fa6fea7fd21ff4e764ece9516f4a
root <root@Carrot42>
2022-06-12 10:15:44 -0400

    Added functions for Layered, Ratio, and stacked Graphs.
    Added labels for each graph and cleaned the code.
    
    Added comments for explanation for how code functions.

--- exampleMulti.C
+++ exampleMulti.C
@@ -8,22 +8,40 @@
 void exampleMulti()
 {
 
-	TFile* f0 = TFile::Open("valid.hist.root", "read");
-	TFile* f1 = TFile::Open("prod5.root", "read");
-	TFile* f2 = TFile::Open("prod5p1.root", "read");
+//Loading each root file
+
+	//TFile* f0 = TFile::Open("valid.hist.root", "read");
+	//TFile* f1 = TFile::Open("prod5.root", "read");
+	//TFile* f2 = TFile::Open("prod5p1.root", "read");
+
+//Creating vector list of each file
 
 	std::vector<string> files;
 	files.push_back("valid.hist.root");
 	files.push_back("prod5.root");
 	files.push_back("prod5p1.root");
 
-	std::vector<TH1*> hists;
-	hists.push_back(LoadTH1(f0, "validgenie/Enu"));
-	hists.push_back(LoadTH1(f1, "validgenie/numu/Enu"));
-	hists.push_back(LoadTH1(f2, "validgenie/nue/Enu"));
 
+//Creating vector list of each graph wanting to draw
 
-	DrawTH1Indiv(files, hists, kOkabelto1, "plots/");
+	//std::vector<TH1*> hists;
+	//hists.push_back(LoadTH1(f, "validgenie/Enu"));
+	//hists.push_back(LoadTH1(f, "validgenie/numu/Enu"));
+	//hists.push_back(LoadTH1(f, "validgenie/nue/Enu"));
 
+	std::vector<string> bars;
+	bars.push_back("validgenie/Enu");
+	bars.push_back("validgenie/numu/Enu");
+	bars.push_back("validgenie/nue/Enu");
+
+	DrawTH1Indiv(files, bars, kOkabelto1);
+	
+	DrawTH1Multi(files, bars, kOkabelto1);
+
+	DrawTH1MultiLayered(files, bars, kOkabelto1);
+	
+	DrawTH1MultiRatio(files, bars, kOkabelto1);
+
+	DrawTH1MultiStacked(files, bars, kOkabelto1);
 
 }
